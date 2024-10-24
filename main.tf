3b8833e3e36084a9d455781e099c4d33e72af598
psschiatanya <psschiatanya@gmail.com>
2024-10-24 19:14:44 +1100

    update inventory

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.6.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+   # Optional: Specify the subscription ID if needed
+  subscription_id = "b5ab04a8-e99d-4a42-820b-f823d113ed94"
 }
 
 resource "azurerm_resource_group" "example" {
