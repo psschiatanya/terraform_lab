edf123d4bfef745aae3f793557eebeb937d9b849
psschiatanya <psschiatanya@gmail.com>
2024-10-26 00:25:10 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-
+   # Optional: Specify the subscription ID if needed
+  subscription_id = "b5ab04a8-e99d-4a42-820b-f823d113ed94"
 }
+
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
