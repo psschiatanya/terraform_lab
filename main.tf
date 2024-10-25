3f543531bbd355ab159099f9672a3b48ccb094d4
psschiatanya <psschiatanya@gmail.com>
2024-10-26 00:39:16 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.6.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-   # Optional: Specify the subscription ID if needed
-  subscription_id = "b5ab04a8-e99d-4a42-820b-f823d113ed94"
-  tenant_id = "ab5765fe-b3c7-439b-916c-e502a47c9617"
-   
 }
-
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
